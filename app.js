8442491a8914530a61769c79cb13cd6799d0461e
maraeliza <marasampaioslm375@gmail.com>
2025-02-07 20:53:43 -0300

    add func sortearAmigo e addAmigo

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+const resultDiv = document.getElementById("resultado");
+const listaAmigosDiv = document.getElementById("listaAmigos");
+const input = document.getElementById("amigo");
+
+function adicionarAmigo() {
+  const nomeInput = input.value;
+
+  if (nomeInput == "") {
+    alert("Insira um nome válido para adicionar");
+    return;
+  } else {
+    input.value = "";
+    listaAmigosDiv.innerHTML = "";
+
+    amigos.push("<li>" + nomeInput + "</li>");
+    listaAmigosDiv.innerHTML = amigos.join("").replace(",", "");
+  }
+}
+
+function sortearAmigo() {
+  const i = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[i];
+  resultDiv.innerHTML = amigoSorteado;
+}
